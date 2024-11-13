5c92f0259c940d4ff25bc7105b6bd444dc0df73a
c4lopsitta <simone@atomtech.cc>
2024-11-13 21:01:22 +0100

    Adding base pages

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,8 @@ LABEL authors="c4lopsitta"
 COPY . /app
 COPY .env /app/.env
 
-
-EXPOSE 4040:4040
 WORKDIR /app
 
-RUN python3 -m venv venv
-RUN source venv/bin/activate
 RUN pip install -r /app/requirements.txt
 
 ENTRYPOINT ["uvicorn", "webpage:app", "--host", "0.0.0.0", "--port", "4040"]
--- webpage.py
+++ webpage.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import santastores
+from santastores import writer_queue
 
 app = FastAPI()
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
@@ -14,26 +15,64 @@ templates = Jinja2Templates(directory="templates")
 @app.get('/')
 def join_store(request: Request,
                name: str = "",
-               email_address: str = "",
+               email_address: str | None = None,
                store_id: str = None):
     if store_id is None:
-        return HTMLResponse(content=templates.TemplateResponse(
+        return templates.TemplateResponse(
             request=request,
-            name="no_store_id.html"
-        ))
+            name="error.html",
+            status_code=404,
+            context={
+                "id": None,
+                "error_text": "A santa store id is required. Ask the organiser for the correct link.",
+                "error_title": "No Store Id",
+                "error_code": "NO_STORE_ID"
+            }
+        )
 
     # there's a store, load it
-    santastore = santastores.load_store(store_id)
+    try:
+        santastore = santastores.load_store(store_id)
+    except Exception as e:
+        print(e.__str__())
+        return templates.TemplateResponse(
+            request=request,
+            name="error.html",
+            status_code=404,
+            context={
+                "id": store_id,
+                "error_title": "Invalid store id",
+                "error_code": "INVALID_STORE_ID",
+                "error_text": f"The given id {store_id} was not found."
+            }
+        )
 
-    return HTMLResponse(content=templates.TemplateResponse(
+    return templates.TemplateResponse(
         request=request,
         name="join_store.html",
         context={
+            "id": store_id,
             "store_id": store_id,
             "name": name,
             "email_address": email_address
         }
-    ))
+    )
+
+@app.post("/")
+def store_data(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        status_code=202,
+        name="register.html",
+    )
+
+
+@app.get("/confirm_email")
+def confirm_email(request: Request,
+                  name: str,
+                  email_address: str,
+                  store_id: str | None = None,):
+    pass
 
 
 
